10da42bea367f9299cda93f03dc113442f68ef2b
Ricky Jonathan <jonathanricky12@gmail.com>
2025-07-13 19:22:18 +0200

    change footer color

--- src/app/(client-specific)/reception/frendy-nia/footer/page.tsx
+++ src/app/(client-specific)/reception/frendy-nia/footer/page.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return <div className="text-center font-lastik text-[#43423D]">
-    <div className="bg-[#F8F8F7] pt-5 pb-4 md:pt-6 md:pb-8 lg:pt-8 lg:py-10">
+    <div className="bg-[#F8F9F5] pt-5 pb-4 md:pt-6 md:pb-8 lg:pt-8 lg:py-10">
       <h5 className="text-[12px] md:text-[14px] lg:text-[16px]">
         Amarento
       </h5>
