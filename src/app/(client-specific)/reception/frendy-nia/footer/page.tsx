0a19f7f441f07f6f2d5c68c7dc2660d2717d39d6
Ricky Jonathan <jonathanricky12@gmail.com>
2025-07-13 13:21:21 +0200

    add footer

--- src/app/(client-specific)/reception/frendy-nia/footer/page.tsx
+++ src/app/(client-specific)/reception/frendy-nia/footer/page.tsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <div />;
+  return <div className="text-center font-lastik text-[#43423D]">
+    <div className="bg-[#F8F8F7] pt-5 pb-4 md:pt-6 md:pb-8 lg:pt-8 lg:py-10">
+      <h5 className="text-[12px] md:text-[14px] lg:text-[16px]">
+        Amarento
+      </h5>
+      <p className="text-[8px] md:text-[10px] lg:text-[12px]">
+        Copyright © 2025. All rights reserved.
+      </p>    
+    </div>
+  </div>;
 }
