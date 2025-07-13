2c0faf9cffb410f76342b5dd01f4b5425af1c235
Felix Arjuna <felixarjuna.de@yahoo.com>
2025-07-13 10:12:06 +0200

    feat: update holmat page

--- src/app/(client-specific)/holmat/frendy-nia/page.tsx
+++ src/app/(client-specific)/holmat/frendy-nia/page.tsx
@@ -1,15 +1,21 @@
-'use client';
-
-import { Gift } from 'lucide-react';
+import { getGuestNameByIdAction } from '~/server/actions';
 import Bibleverse from '../../reception/frendy-nia/bibleverse/page';
 import Brideandgroom from '../../reception/frendy-nia/brideandgroom/page';
+import Gift from '../../reception/frendy-nia/gift/page';
 import Holymatrimony from '../../reception/frendy-nia/holymatrimony/page';
 import Homepage from '../../reception/frendy-nia/homepage/page';
 import Photoalbumone from '../../reception/frendy-nia/photoalbumone/page';
 import Thewedding from '../../reception/frendy-nia/thewedding/page';
 import Wish from '../../reception/frendy-nia/wish/page';
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { guestId: string };
+}) {
+  const guestId = Number.parseInt(searchParams?.guestId, 10);
+  const guestName = await getGuestNameByIdAction(guestId);
+
   return (
     <div>
       <Homepage />
@@ -20,7 +26,7 @@ export default function Page() {
       <Photoalbumone />
       <Holymatrimony />
       <Gift />
-      <Wish />
+      <Wish guestId={guestId} guestName={guestName} />
     </div>
   );
 }
