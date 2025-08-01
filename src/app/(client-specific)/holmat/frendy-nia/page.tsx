e7760402469d1a62884f41cb837f6c4d1ac07d2f
Ricky Jonathan <jonathanricky12@gmail.com>
2025-08-01 17:34:25 +0200

    fix import wish

--- src/app/(client-specific)/holmat/frendy-nia/page.tsx
+++ src/app/(client-specific)/holmat/frendy-nia/page.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
-import { useServerActionQuery } from '~/lib/hooks/server-action-hooks';
-import { getGuestNameByIdAction } from '~/server/actions';
-import Bibleverse from '../../reception/frendy-nia/bibleverse/page';
-import Photoalbumone from '../../reception/frendy-nia/photoalbumone/page';
-import Brideandgroom from '../../reception/frendy-nia/brideandgroom/page';
-import Gift from '../../reception/frendy-nia/gift/page';
-import Holymatrimony from '../../reception/frendy-nia/holymatrimony/page';
-import Homepage from '../../reception/frendy-nia/homepage/page';
-import Photoalbumtwo from '../../reception/frendy-nia/photoalbumtwo/page';
-import Thewedding from '../../reception/frendy-nia/thewedding/page';
-import Wish from '../../reception/frendy-nia/wish/wish';
-import Footer from '../../reception/frendy-nia/footer/page';
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { useServerActionQuery } from "~/lib/hooks/server-action-hooks";
+import { getGuestNameByIdAction } from "~/server/actions";
+import Bibleverse from "../../reception/frendy-nia/bibleverse/page";
+import Photoalbumone from "../../reception/frendy-nia/photoalbumone/page";
+import Brideandgroom from "../../reception/frendy-nia/brideandgroom/page";
+import Gift from "../../reception/frendy-nia/gift/page";
+import Holymatrimony from "../../reception/frendy-nia/holymatrimony/page";
+import Homepage from "../../reception/frendy-nia/homepage/page";
+import Photoalbumtwo from "../../reception/frendy-nia/photoalbumtwo/page";
+import Thewedding from "../../reception/frendy-nia/thewedding/page";
+import Wish from "../../reception/frendy-nia/wish/page";
+import Footer from "../../reception/frendy-nia/footer/page";
 
 export default function Page() {
   return (
@@ -26,13 +26,13 @@ export default function Page() {
 function PageContent() {
   const searchParams = useSearchParams();
   // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
-  const guestId = Number.parseInt(searchParams.get('guestId') as string, 10);
+  const guestId = Number.parseInt(searchParams.get("guestId") as string, 10);
 
   const { data: guestName } = useServerActionQuery(getGuestNameByIdAction, {
     input: {
       guestId,
     },
-    queryKey: ['guest-name'],
+    queryKey: ["guest-name"],
   });
 
   return (
